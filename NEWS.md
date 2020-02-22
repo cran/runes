d5105f4f5299978d321ef72a2d70691faa9d75bf
Bryan Jenks <bryanjenks@protonmail.com>
2020-02-22 09:20:03 +0000

    version 0.0.1

---
+++ NEWS.md
@@ -0,0 +1,91 @@
+# Changelog
+
+All notable changes to this project will be documented in this file.
+
+The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
+and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
+
+## Summary
+
+> Given a version number MAJOR.MINOR.PATCH, increment the:
+>
+> 1. MAJOR version when you make incompatible API changes,
+> 2. MINOR version when you add functionality in a backwards compatible manner, and
+> 3. PATCH version when you make backwards compatible bug fixes.
+>
+> Additional labels for pre-release and build metadata are available as extensions to the MAJOR.MINOR.PATCH format.
+
+### Guiding Principles
+
+> Changelogs are for _humans_, not machines.
+> There should be an entry for every single version.
+> The same types of changes should be grouped.
+> Versions and sections should be linkable.
+> The latest version comes first.
+> The release date of each version is displayed.
+> Mention whether you follow [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
+
+### Types of changes
+
+> Added for new features.
+> Changed for changes in existing functionality.
+> Deprecated for soon-to-be removed features.
+> Removed for now removed features.
+> Fixed for any bug fixes.
+> Security in case of vulnerabilities.
+
+## [Unreleased]
+
+### Added
+
+-
+
+### Changed
+
+-
+
+### Deprecated
+
+-
+
+### Removed
+
+-
+
+### Fixed
+
+-
+
+### Security
+
+-
+
+## [0.0.1] - 2020-01-22
+
+### Added
+
+- Core functionality of a-z alphabet translation to Elder Futhark
+- all special characters, numeric, or otherwise all ignored as pass through
+- New function `runes_table()` create a 3 column data frame with the unicode sequence, transcription, and character of the entire Elder Futhark alphabet to be used for inputting into documents, reference, or any other purpose
+
+### Changed
+
+- new parameter to `runes()` `hide=FALSE` is now the default option but when set to true, the English 'x' & 'q' characters will not pass through the function and appear at all since there is no equivalent rune.
+    + added unit tests to support this new parameter option
+    
+### Deprecated
+
+-
+
+### Removed
+
+-
+
+### Fixed
+
+-
+
+### Security
+
+-
+
---
+++ R/runes.R
@@ -0,0 +1,57 @@
+#' @title
+#' runes()
+#'
+#' @description
+#' `runes()` returns the input string argument with all 'A-Z' characters
+#' replaced by Elder Futhark Runes. You can use this in console output, scripts,
+#' RMarkdown documents, anything with Unicode/UTF-8 support
+#'
+#' @param x a character string
+#'
+#' @param hide boolean option to hide non translated English alphabet
+#' characters from output. Default option hides nothing from user
+#'
+#' @return If input is a single character string then output will be a character
+#' string with unicode Elder Futhark runes replacing the 'A-Z' characters except
+#' 'q' and 'x'
+#'
+#' @examples
+#' cat(runes("Your Results Are: "), 7 * 49, "\n")
+#'
+#' runes("hello world")
+#'
+#' @export
+runes <- function(x, hide=FALSE) {
+
+    base::stopifnot(base::class(x) == base::class("character"))
+
+        input_string <- base::tolower(x)
+
+        output_string <- base::gsub(pattern = "th", x = input_string, replacement = "\u16a6")
+        output_string <- base::gsub(pattern = "ng", x = output_string, replacement = "\u16dc")
+        output_string <- base::gsub(pattern = "[wv]", x = output_string, replacement = "\u16b9")
+        output_string <- base::gsub(pattern = "[ck]", x = output_string, replacement = "\u16b2")
+        output_string <- base::gsub(pattern = "[jy]", x = output_string, replacement = "\u16c3")
+        output_string <- base::gsub(pattern = "a", x = output_string, replacement = "\u16a8")
+        output_string <- base::gsub(pattern = "b", x = output_string, replacement = "\u16d2")
+        output_string <- base::gsub(pattern = "d", x = output_string, replacement = "\u16de")
+        output_string <- base::gsub(pattern = "e", x = output_string, replacement = "\u16d6")
+        output_string <- base::gsub(pattern = "f", x = output_string, replacement = "\u16a0")
+        output_string <- base::gsub(pattern = "g", x = output_string, replacement = "\u16b7")
+        output_string <- base::gsub(pattern = "h", x = output_string, replacement = "\u16bb")
+        output_string <- base::gsub(pattern = "i", x = output_string, replacement = "\u16c1")
+        output_string <- base::gsub(pattern = "l", x = output_string, replacement = "\u16da")
+        output_string <- base::gsub(pattern = "m", x = output_string, replacement = "\u16d7")
+        output_string <- base::gsub(pattern = "n", x = output_string, replacement = "\u16be")
+        output_string <- base::gsub(pattern = "o", x = output_string, replacement = "\u16df")
+        output_string <- base::gsub(pattern = "p", x = output_string, replacement = "\u16c8")
+        output_string <- base::gsub(pattern = "q", x = output_string, replacement = if (hide) {""} else {"q"})
+        output_string <- base::gsub(pattern = "r", x = output_string, replacement = "\u16b1")
+        output_string <- base::gsub(pattern = "s", x = output_string, replacement = "\u16cb")
+        output_string <- base::gsub(pattern = "t", x = output_string, replacement = "\u16cf")
+        output_string <- base::gsub(pattern = "u", x = output_string, replacement = "\u16a2")
+        output_string <- base::gsub(pattern = "x", x = output_string, replacement = if (hide) {""} else {"x"})
+        output_string <- base::gsub(pattern = "z", x = output_string, replacement = "\u16c9")
+
+    return(output_string)
+}
